00efc829126e034ed029d588df90315c00b9b5b6
dangerousor <1192350292@qq.com>
2019-04-25 18:15:45 +0800

    captcha

--- spider.py
+++ spider.py
@@ -81,6 +81,7 @@ class Spider:
         }
         html = self.post_html(url, data).json()
         if html['Code'] == 1:
+            print(html)
             return
         else:
             if html['Message'] == '验证码输入有误' or html['Message'] == '验证码错误':
