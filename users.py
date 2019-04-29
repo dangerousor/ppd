0933d9873818105890d844182e71455e1155e7c3
dangerousor <1192350292@qq.com>
2019-04-29 16:08:11 +0800

    account

--- users.py
+++ users.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 user = {
-    'username': '13122317558',
-    'password': '13122317558jlw',
+    'username': '18616019747',
+    'password': 'zsy19970909',
 }
